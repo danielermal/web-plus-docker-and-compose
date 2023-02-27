429d851151e47fad8fc1f9dd4f37d7087de847af
danielermal <ermalyukd@yandex.ru>
2023-02-27 20:47:01 +0700

    deploy

--- backend/src/configuration/configuration.ts
+++ backend/src/configuration/configuration.ts
@@ -6,7 +6,7 @@ export default () => ({
 
   database: {
     type: process.env.DATABASE_TYPE || 'postgres',
-    host: process.env.DATABASE_HOST || 'localhost',
+    host: process.env.DATABASE_HOST || 'database',
     port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
     username: process.env.DATABASE_USERNAME || 'student',
     password: process.env.DATABASE_PASSWORD || 'student',
