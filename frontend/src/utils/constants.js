3d0146a4d14cfd5c0dced72e61f317abcd5bd3b9
danielermal <ermalyukd@yandex.ru>
2023-02-27 22:24:32 +0700

    deploy

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://api.kpd.ermalyukd.nomoredomains.work";
+export const URL = "https://api.kpd.ermalyukd.nomoredomains.work";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
